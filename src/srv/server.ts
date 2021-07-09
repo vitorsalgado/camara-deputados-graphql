9870f352d7273c272a7ee9f2d115ea0e68692853
Vitor Hugo Salgado <vsalgadopb@gmail.com>
2021-07-09 00:23:50 -0400

    feat(server): add correlation id and use GraphiQL
    Signed-off-by: Vitor Hugo Salgado <vsalgadopb@gmail.com>

--- src/srv/server.ts
+++ src/srv/server.ts
@@ -1,29 +1,43 @@
 import { Server } from 'http'
-import Fastify, { FastifyInstance, FastifyServerOptions } from 'fastify'
+import Fastify, { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
 import Mercurius from 'mercurius'
+import { v4 as UUIDv4 } from 'uuid'
 import { Configurations } from '@app/config'
 import { buildSchemas, configureRoutes, graphqlFeatures } from '@app/features'
 import { GracefulShutdownPlugin } from '@app/srv/server.shutdown'
 import Logger from '@app/utils/log'
 
+const buildContext = async (_req: FastifyRequest, _reply: FastifyReply) => ({
+  correlationId: UUIDv4()
+})
+
+type PromiseType<T> = T extends PromiseLike<infer U> ? U : T
+
+declare module 'mercurius' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  interface MercuriusContext extends PromiseType<ReturnType<typeof buildContext>> {}
+}
+
 export class AppServer {
-  private fastifyApp!: FastifyInstance
+  private readonly fastifyApp: FastifyInstance
   private readonly port: number
   private readonly host: string
 
   constructor(private readonly configurations: Configurations) {
     this.port = configurations.server.port
     this.host = configurations.server.host
+    this.fastifyApp = Fastify<Server>({ logger: this.configurations.server.loggerEnabled })
   }
 
-  build(opts?: FastifyServerOptions): FastifyInstance {
-    this.fastifyApp = Fastify<Server>({ logger: this.configurations.server.loggerEnabled, ...opts })
+  build(): FastifyInstance {
     this.fastifyApp.register(GracefulShutdownPlugin)
 
     const features = graphqlFeatures(this.configurations)
 
     this.fastifyApp.register(Mercurius, {
-      graphiql: this.configurations.gql.playground,
+      graphiql: this.configurations.graphql.graphiql,
+      onlyPersisted: false,
+      context: buildContext,
       schema: buildSchemas(),
       resolvers: features.Resolvers,
       loaders: features.Loaders
@@ -34,7 +48,9 @@ export class AppServer {
     return this.fastifyApp
   }
 
-  server = (): Server => this.fastifyApp.server
+  server(): Server {
+    return this.fastifyApp.server
+  }
 
   async start(): Promise<void> {
     await this.fastifyApp.ready()
@@ -48,14 +64,14 @@ export class AppServer {
       })
   }
 
-  async buildAndStart(opts?: FastifyServerOptions): Promise<FastifyInstance> {
-    await this.build(opts)
+  async buildAndStart(): Promise<FastifyInstance> {
+    await this.build()
     await this.start()
 
     return this.fastifyApp
   }
 
   async close(): Promise<void> {
-    await this.fastifyApp.close()
+    return this.fastifyApp.close()
   }
 }
